1a33e2420156697a107c58dbff04ad5a16784a91
Guyllem <guyllem.boyer@gmail.com>
2025-03-15 10:56:44 +0100

    Rename gestion_entreprises_pilotes.html to gestion_entreprises_pilotes.php; refactor navbar and filter sections for improved functionality and layout

--- pages/page_pilote/gestion_entreprises_pilotes.php
+++ pages/page_pilote/gestion_entreprises_pilotes.php
@@ -7,13 +7,36 @@
   <link rel="stylesheet" href="styles_page_pilote.css">
 </head>
 <body>
-  <nav>
-    <div class="logo">Stage Connect</div>
-    <div class="nav-right">
-      <a href="#" class="nav-item">Se déconnecter</a>
-    </div>
-  </nav>
+    <?php
+    $GLOBALS['responsiveFilter'] = true;
+    include 'navbar-pilote.php';
+    ?>
 
+    <div class="slide-filter" id="slide-filter">
+        <div class="slide-filter">
+        <h3>Filtres</h3>
+        </div>
+            <div class="slide-filter-section">
+                <h4>Localisation :</h4>
+                <input type="text" placeholder="Ville, Code Postal, Département, Région" class="input-location">
+            </div>
+            <div class="slide-filter-section">
+                <h4>Secteur :</h4>
+                <input type="text" placeholder="Informatique, BTP, Industrie" class="input-skills">
+            </div>
+            <h4>Nombres d'offres :</h4>
+            <div class="slide-radio-group">
+                <label><input type="radio" name="sort"> Croissant</label>
+                <label><input type="radio" name="sort"> Décroissant</label>
+            </div>
+            <h4>Popularité :</h4>
+            <div class="slide-radio-group">
+                <label><input type="radio" name="sort"> Croissant</label>
+                <label><input type="radio" name="sort"> Décroissant</label>
+            </div>
+        <button class="slide-apply-btn">Appliquer</button>
+        <button class="close-filter" id="close-filter">✖</button>
+    </div>
   <main>
       <div class="filters">
         <h3>Filtres</h3>
@@ -34,32 +57,19 @@
                 <label><input type="radio" name="sort"> Croissant</label>
                 <label><input type="radio" name="sort"> Décroissant</label>
             </div>
-            <br>
             <h4>Popularité :</h4>
             <div class="radio-group">
                 <label><input type="radio" name="sort"> Croissant</label>
                 <label><input type="radio" name="sort"> Décroissant</label>
             </div>
         </div>
-        <br>
-        <br>
-        <br>
-        <br>
         <button class="apply-btn">Appliquer</button>
-
-        <br> <br> <br> <br>
       </div>
-    
+
       <div class="main-content">
-      </div> 
-    
+      </div>
   </main>
-  <footer>
-    <p id="footer-text"> © 2025 - Web4all - Tous droits réservés. |
-      <a href="../Mentions_legales/mentions_legales.html">Mentions légales</a> |
-      <a href="../Mentions_legales/politiques_de_confidentialité.html">Politique de confidentialité</a> |
-      <a href="../Mentions_legales/condition_d'utilisation.html">Conditions d'utilisation</a>
-    </p>
-  </footer>
+  <?php include '../page_etudiant/footer.php'; ?>
 </body>
+<script src="../page_etudiant/etudiant_script.js"></script>
 </html>
