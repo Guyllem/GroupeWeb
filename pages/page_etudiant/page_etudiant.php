0a2197ed30a79898fdb4168f6f334e6129675af5
Guyllem <guyllem.boyer@gmail.com>
2025-03-18 09:54:48 +0100

    add new card_offre.php template

--- pages/page_etudiant/page_etudiant.php
+++ pages/page_etudiant/page_etudiant.php
@@ -169,9 +169,7 @@
                 </svg>
               </div>
             </div>
-
-            <button class="view-btn">Regarder</button>
-
+              <button class="view-btn" onclick="window.location.href='card_offre.php'">Regarder</button>
           </div>
         </div>
       </div>
