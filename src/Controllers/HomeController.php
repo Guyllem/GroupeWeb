05baee79ed4807bf0d5f0506d129083e8b30e792
JustAeris <aeris.github@gmail.com>
2025-03-27 09:28:10 +0100

    Added base controller/router structure

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,26 +1,51 @@
 <?php
 namespace App\Controllers;
 
-class HomeController {
-    private $twig;
-    private $db;
-
+class HomeController extends BaseController {
     public function __construct($twig, $db) {
-        $this->twig = $twig;
-        $this->db = $db;
+        parent::__construct($twig, $db);
     }
 
     public function index() {
-        echo "Hello World! This is the home page.";
-        // Or with Twig:
-        // echo $this->twig->render('home/index.html.twig', [
-        //     'message' => 'Hello World!'
-        // ]);
+        // Rediriger vers la page de connexion si non connecté
+        $currentUser = $this->getAuth()->getCurrentUser();
+        if (!$currentUser) {
+            header('Location: /login');
+            exit;
+        }
+
+        // Rediriger en fonction du type d'utilisateur
+        $userType = $this->getAuth()->getUserType();
+        switch ($userType) {
+            case 'admin':
+                header('Location: /admin');
+                break;
+            case 'pilote':
+                header('Location: /pilotes');
+                break;
+            case 'etudiant':
+                header('Location: /etudiant');
+                break;
+            default:
+                header('Location: /login');
+                break;
+        }
+        exit;
     }
 
     public function about() {
-        echo "This is the about page.";
-        // Or with Twig:
-        // echo $this->twig->render('home/about.html.twig');
+        echo $this->twig->render('home/about.html.twig');
+    }
+
+    public function mentionsLegales() {
+        echo $this->twig->render('home/mentions_legales.html.twig');
+    }
+
+    public function politiqueConfidentialite() {
+        echo $this->twig->render('home/politique_confidentialite.html.twig');
+    }
+
+    public function conditionsUtilisation() {
+        echo $this->twig->render('home/conditions_utilisation.html.twig');
     }
 }
